94e355399413f21486bb595b7fa32718d90d517b
Juan Diego Guevara <39684454+JDjuxx@users.noreply.github.com>
2019-12-01 21:13:46 -0500

    Clases 28 y 29 nov

--- http-prueba-1/src/app.controller.ts
+++ http-prueba-1/src/app.controller.ts
@@ -1,27 +1,70 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Headers, HttpCode, InternalServerErrorException, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('juxxi')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @HttpCode(200)
+
+  @Post()
+  byeWorld(): string {
+    const seconds = this.getSeconds();
+    if (seconds % 2 === 0) {
+      return 'Adios Mundo!';
+    } else {
+      throw new InternalServerErrorException(
+        'Es impar',
+      );
+    }
+
+  }
+
+  private getSeconds(): number {
+    return new Date().getSeconds();
+  }
+  //parametros de ruta = oligatorio //parametros de consulta = no obligatorios
+  @Get('inscripcion-curso/:idCurso/:cedula') // /:nombreDelParametro
+  public inscripcionCurso(
+    // Siempre tipar cualquier cosa que usamos
+    @Param() parametrosDeRuta:ObjetoInscripcion,
+    @Param('idCedula') idCedula:string,
+    @Param('cedula') cedula:string
+  ): string {
+    console.log(parametrosDeRuta);
+    // template strings \\ `Mensaje ${variable}`
+    return `Te inscribiste al curso: ${parametrosDeRuta.idCurso} 
+    Cedula: ${parametrosDeRuta.cedula}`;
+  }
+
+  @Post('almorzar')
+  @HttpCode(200)
+  public almorzar(
+    @Body() parametroDeCuerpo,
+    @Body('id') id:number
+  ):string{
+    console.log(parametroDeCuerpo);
+    return `Te inscribiste al curso ${parametroDeCuerpo}`
+  }
+
+  @Get('obtener-cabeceras')
+  obtenerCabeceras(
+    @Headers() cabeceras,
+    @Headers('numeroUno') numeroUno: string
+  ): string{
+    console.log(cabeceras);
+    return `Las cabeceras son: ${cabeceras}`;
+  }
+
 }
 
-let surname: string = 'Guevara';
-const cedula: string = '060...';
-
-surname = 'Sanandres';
-
-const married: boolean = false;
-const age: number = 21;
-const salary: number = 3.94;
-let hijos = 0;
-hijos = null;
-// tslint:disable-next-line:prefer-const
-let ojos;
-if (married === false){
+interface ObjetoInscripcion{
+  idCurso: string;
+  cedula: string;
 }
